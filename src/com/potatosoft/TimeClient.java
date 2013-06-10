113768f68d3ff281bb9b4c8ef4b8e5ef641342b7
Wonhee Jung <wonhee.jung@gmail.com>
2013-06-09 21:12:00 -0700

    Added client
    Added SOAP WebService client. Changed server listening port from 8081
    to 5000.

---
+++ src/com/potatosoft/TimeClient.java
@@ -0,0 +1,24 @@
+package com.potatosoft;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+public class TimeClient {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		URL url = new URL("http://localhost:5000/?wsdl");
+		QName qname = new QName("http://potatosoft.com/", "TimeServerImplService");
+		
+		Service service = Service.create(url, qname);
+		
+		TimeServer eif = service.getPort(TimeServer.class);
+		
+		System.out.println(eif.getTimeAsString());
+		System.out.println(eif.getTimeAsElapsed());
+	}
+
+}
--- src/com/potatosoft/TimeServerPublisher.java
+++ src/com/potatosoft/TimeServerPublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class TimeServerPublisher {
 
 	public static void main(String[] args) {
-		Endpoint.publish("http://127.0.0.1:8081/", new TimeServerImpl());
+		Endpoint.publish("http://127.0.0.1:5000/", new TimeServerImpl());
 	}
 
 }
